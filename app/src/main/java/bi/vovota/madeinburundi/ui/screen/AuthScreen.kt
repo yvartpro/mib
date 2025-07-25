281a4c700268318235cbbfa505aa4f24e99c2dd5
kwakuvovota <vovotadevs@gmail.com>
2025-07-25 04:12:11 -0400

    Scroll on login fixed and even added verify password

--- app/src/main/java/bi/vovota/madeinburundi/ui/screen/AuthScreen.kt
+++ app/src/main/java/bi/vovota/madeinburundi/ui/screen/AuthScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
@@ -90,7 +91,8 @@ userViewModel: UserViewModel
       .fillMaxSize()
       .padding(0.dp)
       .background(color = MaterialTheme.colorScheme.background )
-      .verticalScroll(rememberScrollState()),
+      .verticalScroll(rememberScrollState())
+      .imePadding(),
     verticalArrangement = Arrangement.Top,
     horizontalAlignment = Alignment.CenterHorizontally
   ) {
